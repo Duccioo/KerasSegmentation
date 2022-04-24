eab531e9b4fa24b236c0bef98c87d95dc8e7f4bf
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-04-24 14:53:48 +0200

    Update main_test.py

--- code/main_test.py
+++ code/main_test.py
@@ -11,16 +11,15 @@ from IPython.display import Image
 parser = argparse.ArgumentParser()
 
 #cartelle contenenti immagini, maschere e checkpoint
-parser.add_argument('--img', dest='img_path', type=str, default='train')
-parser.add_argument('--checkpoint', dest='checkpoint_path', type=str, default='train')
+parser.add_argument('--img', dest='img_path', type=str)
+parser.add_argument('--checkpoint', dest='checkpoint_path', type=str)
 
 args = parser.parse_args()
 
 #----------------------------------------------#Main#---------------------------------------------------------#
 
 if __name__ == '__main__':
-  
-    
+
     out = predict(
         checkpoints_path= args.checkpoint_path,
         inp=args.img_path,
