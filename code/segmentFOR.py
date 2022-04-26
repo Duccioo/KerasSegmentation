9dfcf7495fe222b5fc259f2945bfe99727ab9122
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-04-26 13:42:31 +0200

    Update segmentFOR.py

--- code/segmentFOR.py
+++ code/segmentFOR.py
@@ -93,6 +93,7 @@ def compute_confusion_matrix(inputs,target):
     inputs1=inputs.reshape(-1)
     target1=target.reshape(-1)
     CM = confusion_matrix(target1,inputs1)
+    print("1:", CM[0][0]," 2:",CM[0][1]," 3:",CM[1][0]," 4:",CM[1][1])
     
     if CM.ndim==1:
       TN=CM[0][0]
