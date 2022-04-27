ebfb87881b2a63a2e33bd8ec03efba337709dab4
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-04-27 17:37:50 +0200

    Update BW.py

--- code/BW.py
+++ code/BW.py
@@ -14,8 +14,7 @@ def convert_BW(img, path='', save=False):
         if (item[0]==0 and item[1]==0 and item[2]==255):
           new_image_data.append((0, 0, 0))
 
-        if (item[0]==255 and item[1]==255 and item[2]==255):
-          print("#")
+        
         else:
           new_image_data.append(item)
           
