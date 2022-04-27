5420265a001669329c539f60c4ac996e72dbcec4
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-04-27 17:21:51 +0200

    Update BW.py

--- code/BW.py
+++ code/BW.py
@@ -14,12 +14,12 @@ def convert_BW(img, path='', save=False):
         if (item[0]==0 and item[1]==0 and item[2]==255):
           new_image_data.append((0, 0, 0))
 
-        if (item[0]==255 and item[1]==255 and item[2]==255):
-          print(". ")
+        if (item[0]==0 and item[1]==255 and item[2]==246):
+          new_image_data.append((255, 255, 255))
         else:
           new_image_data.append(item)
           
-        print(item)
+        #print(item)
 
     img.putdata(new_image_data)
     #(opzionale) salvo l'immagine convertita in bianco e nero
