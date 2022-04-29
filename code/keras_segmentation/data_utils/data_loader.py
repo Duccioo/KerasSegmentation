009d73ae101aab49decc7510edfe27e45544d44c
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-04-29 21:27:48 +0200

    Update data_loader.py

--- code/keras_segmentation/data_utils/data_loader.py
+++ code/keras_segmentation/data_utils/data_loader.py
@@ -66,17 +66,18 @@ def get_pairs_from_text(img_path,text_path, other_inputs_paths=None):
     for line in (file):
         #popola image_files
         img_file=line.split(" ")[0]
-        print(img_file)
+        
         if os.path.isfile(os.path.join(img_path, img_file)) and \
                 os.path.splitext(img_file)[1] in ACCEPTABLE_IMAGE_FORMATS:
             file_name, file_extension = os.path.splitext(img_file)
             image_files.append((file_name, file_extension,
                                 os.path.join(img_path,img_file)))
             image_files.append(img_file)
+            print(image_files)
 
         #popola segmentation_files
         seg_file=line.split(" ")[1]
-        print(seg_file)
+        
         if os.path.isfile(os.path.join(img_path, seg_file)) and \
         os.path.splitext(seg_file)[1] in ACCEPTABLE_SEGMENTATION_FORMATS:
             file_name, file_extension = os.path.splitext(seg_file)
@@ -89,7 +90,8 @@ def get_pairs_from_text(img_path,text_path, other_inputs_paths=None):
                                         .format(file_name, full_dir_entry))
 
             segmentation_files[file_name] = (file_extension, full_dir_entry)
-
+            print(image_files)
+            
     if other_inputs_paths is not None:
             other_inputs_files = []
 
