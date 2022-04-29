4c51087ea47dc25d9eeb51db5a3f4a7bee1cf267
Duccioo <meconcelliduccio@hotmail.it>
2022-04-29 12:09:01 +0200

    Update predict.py

--- code/keras_segmentation/predict.py
+++ code/keras_segmentation/predict.py
@@ -161,11 +161,11 @@ def predict(model=None, inp=None, out_fname=None,
     x = get_image_array(inp, input_width, input_height,
                         ordering=IMAGE_ORDERING)
     pr = model.predict(np.array([x]))[0]
-    print("dimensioni pr",pr.shape())
+    print("dimensioni pr",np.shape(pr))
 
 
     pr = pr.reshape((output_height,  output_width, n_classes)).argmax(axis=2)
-    print("dimensioni pr",pr.shape())
+    print("dimensioni pr",np.shape(pr))
     
     seg_img = visualize_segmentation(pr, inp, n_classes=n_classes,
                                      colors=colors, overlay_img=overlay_img,
