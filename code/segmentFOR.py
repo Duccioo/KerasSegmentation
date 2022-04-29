28c4b71e3e95efb77f616f188045a6dad6673631
Duccioo <meconcelliduccio@hotmail.it>
2022-04-29 12:34:58 +0200

    Update segmentFOR.py

--- code/segmentFOR.py
+++ code/segmentFOR.py
@@ -182,7 +182,7 @@ for file in glob.glob("*.jpg"): #ciclo le immagini dentro la cartella
   
   print(in_mask.shape,y_out.shape)
   #mi calcolo gli indici che mi servono
-  DICE=1-dice_loss(y_out,in_mask)
+  DICE=1-dice_loss(y_out/255,in_mask/255)
   JACCARDB=jaccard_binary(in_mask/255, y_out/255)
 
   #(opzionale) posso scartare le immagini che ritornano 1.0 con DICE, ovvero le immagini completamente nere, selezionando no1
