2c8feb51207e7089aa79191203fdc6cdc3346234
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-04-29 19:11:02 +0200

    Update main_train.py

--- code/main_train.py
+++ code/main_train.py
@@ -43,8 +43,8 @@ if __name__ == '__main__':
 			epochs=args.epoch,
 			steps_per_epoch=args.step_epoch,
 			auto_resume_checkpoint=args.autoresume,
-			verify_dataset=False,
-			validate=True
+			verify_dataset=False
+			
 		)
 
 	else: 
