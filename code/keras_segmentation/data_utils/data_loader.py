c909eef9eaabc403834fbed86bad79786ca34ed5
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-05-03 16:10:58 +0200

    Update data_loader.py

--- code/keras_segmentation/data_utils/data_loader.py
+++ code/keras_segmentation/data_utils/data_loader.py
@@ -78,6 +78,8 @@ def get_pairs_from_text(img_path,text_path, other_inputs_paths=None):
         seg_name= seg_file.split("/")[2].strip('\n')
         if os.path.isfile((img_path+seg_file).strip()):
             file_name=seg_name.split(".")[0]
+            if file_name.find("_seg")!=-1:
+                file_name=file_name.strip("_seg")
             file_extension=seg_name.split(".")[1]
             full_dir_entry = os.path.join(img_path+"/masks/", seg_name)
             if file_name in segmentation_files:
