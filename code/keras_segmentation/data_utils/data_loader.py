f2a8d506e21326c4931c0edb18b087a1ee5dcb94
Duccioo <86722319+Duccioo@users.noreply.github.com>
2022-05-03 14:06:51 +0200

    Update data_loader.py

--- code/keras_segmentation/data_utils/data_loader.py
+++ code/keras_segmentation/data_utils/data_loader.py
@@ -81,7 +81,7 @@ def get_pairs_from_text(img_path,text_path, other_inputs_paths=None):
         print("prova:",seg_file)
         seg_name= seg_file.split("/")[2]
         
-        print('os:',img_path+seg_file)
+        print('os:',img_path+seg_file.strip())
         if os.path.isfile((img_path+seg_file)):
             file_name=seg_name.split(".")[0]
             print('assas')
